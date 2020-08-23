8bff27541139ed82845e66a9f55c5ca9516de548
Richard Snazell <rsnazell@richards-mbp.lan>
2020-08-23 23:36:54 +1000

    remove @todo comment

--- src/components/weather/Weather.constants.ts
+++ src/components/weather/Weather.constants.ts
@@ -1,6 +1,6 @@
 import { MeasurementUnit } from "./Weather.types";
 
-export const APPID = "5debbf2f87fa13a9ff3183d7b91f2f20"; // @todo: move from /src to local .env / server variable
+export const APPID = "5debbf2f87fa13a9ff3183d7b91f2f20";
 
 export const UNITS: { [key: string]: MeasurementUnit } = Object.freeze({
   METRIC: "metric",
