affd3c5251dbf59484da0553d19a8350c40e6bc8
Richard Snazell <rsnazell@richards-mbp.lan>
2020-08-29 12:29:29 +1000

    use https endpoint

--- src/components/weather/Weather.constants.ts
+++ src/components/weather/Weather.constants.ts
@@ -1,6 +1,6 @@
 import { MeasurementUnit } from "./Weather.types";
 
-export const OWM_URL = "http://api.openweathermap.org";
+export const OWM_URL = "https://api.openweathermap.org";
 
 export const UNITS: { [key: string]: MeasurementUnit } = Object.freeze({
   METRIC: "metric",
